7a39a99fa45db179703a1e2ea8391577ecbe1db8
Tim Trautman <tctrautman@gmail.com>
2017-11-15 11:46:31 -0800

    Render 'Hello World' with webpack-dev-server

---
+++ components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello world</p>
+      </div>
+    );
+  }
+}
+
+export default App;
