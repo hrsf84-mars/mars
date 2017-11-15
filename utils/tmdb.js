fef0eda0817ea531259a6ba4ebfab0c9a443acc4
futamiyuuki <aa805766@gmail.com>
2017-11-15 15:20:39 -0800

    Get images for movie lookup

--- utils/tmdb.js
+++ utils/tmdb.js
@@ -8,7 +8,6 @@ exports.searchMoviesByName = query => {
       query,
     }
   }).then(res => {
-    console.log(res.data);
     return res.data;
   }).catch(err => {
     console.error(err.response.data.status_message);
@@ -21,5 +20,24 @@ exports.fetchMovieById = id => {
       'api_key': process.env.API_KEY,
       // 'language': 'en-US',
     }
-  }).then(res => console.log(res.data)).catch(err => console.error(err.response.data.status_message));
+  }).then(res => {
+    return res.data;
+  }).catch(err => {
+    console.error(err.response.data.status_message);
+  });
+};
+
+exports.fetchImageById = id => {
+  const url = 'https://image.tmdb.org/t/p/w500';
+  return axios.get(`http://api.themoviedb.org/3/movie/${id}/images`, {
+    params: {
+      'api_key': process.env.API_KEY,
+      // 'language': 'en-US',
+    }
+  }).then(res => {
+    const images = res.data.backdrops;
+    return images.map(img => url + img.file_path);
+  }).catch(err => {
+    console.error(err.response.data.status_message);
+  });
 };
