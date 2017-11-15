18f46ae7e0f865d931e85b92f64dd3defc3754a0
futamiyuuki <aa805766@gmail.com>
2017-11-14 19:12:32 -0800

    Experiment with api fetching

--- utils/tmdb.js
+++ utils/tmdb.js
@@ -1,1 +1,19 @@
 const axios = require('axios');
+
+// exports.fetchMovie = () => {
+//   axios.get(`http://api.themoviedb.org/3/discover/movie/${}`, {
+//     params: {
+//       sort_by: ''
+//     }
+//   })
+// };
+
+console.log(process.env.API_KEY);
+
+const movieId = 550;
+
+axios.get(`http://api.themoviedb.org/3/movie/${movieId}`, {
+  params: {
+    'api_key': process.env.API_KEY
+  }
+}).then(res => console.log(res.data)).catch(err => console.error(err));
