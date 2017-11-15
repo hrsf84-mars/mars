b7251a87329f892c664efe27a5dff7d1f467f265
Tim Trautman <tctrautman@gmail.com>
2017-11-15 15:26:47 -0800

    Render single line graph with (slightly transformed) example google analytics data on a single movie

--- components/App.js
+++ components/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Label } from 'recharts';
+import example from './exampleData.js';
+var data = example.data;
 
 class App extends React.Component {
   constructor() {
@@ -8,7 +11,18 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <p>Hello world</p>
+        <h1>Cliff on Mars</h1>
+        <div id="graph">
+          <h1 id="title">Beauty and the Beast</h1>
+          <LineChart width={800} height={400} data={data}>
+          <Line type="monotone" dataKey="value" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" />
+          <XAxis dataKey="formattedAxisTime">
+            <Label value="Weeks after release date" offset={0} position="insideBottom" />
+          </XAxis>
+          <YAxis label={{ value: 'Search Volume', angle: -90, position: 'insideLeft' }} />
+          </LineChart>
+        </div>
       </div>
     );
   }
