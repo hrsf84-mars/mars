94fabacc2322e6673cf8f0b7cca584acfe17f579
Tim Trautman <tctrautman@gmail.com>
2017-11-15 17:14:18 -0800

    Update webpack

--- public/index.js
+++ public/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
 
-
 ReactDOM.render(
-  React.createElement(App),
+  <App />,
   document.getElementById('app')
 );
