e224ed7b83ae6ccc04c22b6b952945852baabbc3
Tim Trautman <tctrautman@gmail.com>
2017-11-20 19:53:45 -0800

    Broaden example data to include both static and longitudinal data for a given movie.  Include more than one data type for graphing.

--- components/App.js
+++ components/App.js
@@ -14,8 +14,8 @@ class App extends React.Component {
         <h1>Cliff on Mars</h1>
         <div id="graph">
           <h1 id="title">Beauty and the Beast</h1>
-          <LineChart width={800} height={400} data={data}>
-          <Line type="monotone" dataKey="value" stroke="#8884d8" />
+          <LineChart width={800} height={400} data={data.longitudinal_data}>
+          <Line type="monotone" dataKey="search_metric" stroke="#8884d8" />
           <CartesianGrid stroke="#ccc" />
           <XAxis dataKey="formattedAxisTime">
             <Label value="Weeks after release date" offset={0} position="insideBottom" />
