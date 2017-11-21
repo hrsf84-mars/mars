f1e8f5151686d9421496e668c40549cd5c529396
Tim Trautman <tctrautman@gmail.com>
2017-11-20 19:53:45 -0800

    Broaden example data to include both static and longitudinal data for a given movie.  Include more than one data type for graphing.

--- components/exampleData.js
+++ components/exampleData.js
@@ -1,46 +1,49 @@
-module.exports.data = [ { time: '1486252800',
-    formattedTime: 'Feb 5 - Feb 11 2017',
-    formattedAxisTime: 'Feb 5',
-    value: 3,
-    formattedValue: [ '3', '84' ] },
-  { time: '1486857600',
-    formattedTime: 'Feb 12 - Feb 18 2017',
-    formattedAxisTime: 'Feb 12',
-    value: 3,
-    formattedValue: [ '3', '85' ] },
-  { time: '1487462400',
-    formattedTime: 'Feb 19 - Feb 25 2017',
-    formattedAxisTime: 'Feb 19',
-    value: 86,
-    formattedValue: [ '4', '86' ] },
-  { time: '1488067200',
-    formattedTime: 'Feb 26 - Mar 4 2017',
-    formattedAxisTime: 'Feb 26',
-    value: 88,
-    formattedValue: [ '6', '88' ] },
-  { time: '1488672000',
-    formattedTime: 'Mar 5 - Mar 11 2017',
-    formattedAxisTime: 'Mar 5',
-    value: 87,
-    formattedValue: [ '8', '87' ] },
-  { time: '1489276800',
-    formattedTime: 'Mar 12 - Mar 18 2017',
-    formattedAxisTime: 'Mar 12',
-    value: 89,
-    formattedValue: [ '19', '89' ] },
-  { time: '1489881600',
-    formattedTime: 'Mar 19 - Mar 25 2017',
-    formattedAxisTime: 'Mar 19',
-    value: 90,
-    formattedValue: [ '24', '90' ] },
-  { time: '1490486400',
-    formattedTime: 'Mar 26 - Apr 1 2017',
-    formattedAxisTime: 'Mar 26',
-    value: 90,
-    formattedValue: [ '15', '90' ] },
-  { time: '1491091200',
-    formattedTime: 'Apr 2 - Apr 8 2017',
-    formattedAxisTime: 'Apr 2',
-    value: 85,
-    formattedValue: [ '9', '85' ] }
-];
+module.exports.data = {
+    _id: 123124987,
+    title: "Beauty and the Beast",
+    product_company: "TBD",
+    genre: ['children'],
+    images: [],
+    revenue: 1238764,
+    estimated_profit: 1264,
+    releast_date: '20171003',
+    longitudinal_data: [
+        {
+          formattedAxisTime: 'Feb 5',
+          search_metric: 84,
+          past_week_revenue: 1239
+        }, {
+          formattedAxisTime: 'Feb 12',
+          search_metric: 85,
+          past_week_revenue: 929
+        }, {
+          formattedAxisTime: 'Feb 19',
+          search_metric: 86,
+          past_week_revenue: 1039
+        }, {
+          formattedAxisTime: 'Feb 26',
+          search_metric: 88,
+          past_week_revenue: 839
+        }, {
+          formattedAxisTime: 'Mar 5',
+          search_metric: 87,
+          past_week_revenue: 1039
+        }, {
+          formattedAxisTime: 'Mar 12',
+          search_metric: 89,
+          past_week_revenue: 1000
+        }, {
+          formattedAxisTime: 'Mar 19',
+          search_metric: 90,
+          past_week_revenue: 987
+        }, {
+          formattedAxisTime: 'Mar 26',
+          search_metric: 90,
+          past_week_revenue: 1469
+        }, {
+          formattedAxisTime: 'Apr 2',
+          search_metric: 85,
+          past_week_revenue: 1399
+        }
+    ]
+};
