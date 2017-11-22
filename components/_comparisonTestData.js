84549ca0894064b3a9530fee889f5309b9c44893
Tim Trautman <tctrautman@gmail.com>
2017-11-21 19:55:59 -0800

    Go back to storing two movie objects and one graphing object, due to setState not working well with storing them in a single object.

---
+++ components/_comparisonTestData.js
@@ -0,0 +1,41 @@
+module.exports.data = {
+    longitudinal_data: [
+        {
+          formattedAxisTime: 'Feb 5',
+          primary_google_trends_vol: 0,
+          secondary_google_trends_vol: 0
+        }, {
+          formattedAxisTime: 'Feb 12',
+          primary_google_trends_vol: 0,
+          secondary_google_trends_vol: 0
+        }, {
+          formattedAxisTime: 'Feb 19',
+          primary_google_trends_vol: 0,
+          secondary_google_trends_vol: 0
+        }, {
+          formattedAxisTime: 'Feb 26',
+          primary_google_trends_vol: 0,
+          secondary_google_trends_vol: 0
+        }, {
+          formattedAxisTime: 'Mar 5',
+          primary_google_trends_vol: 0,
+          secondary_google_trends_vol: 0
+        }, {
+          formattedAxisTime: 'Mar 12',
+          primary_google_trends_vol: 0,
+          secondary_google_trends_vol: 0
+        }, {
+          formattedAxisTime: 'Mar 19',
+          primary_google_trends_vol: 0,
+          secondary_google_trends_vol: 0
+        }, {
+          formattedAxisTime: 'Mar 26',
+          primary_google_trends_vol: 0,
+          secondary_google_trends_vol: 0
+        }, {
+          formattedAxisTime: 'Apr 2',
+          primary_google_trends_vol: 0,
+          secondary_google_trends_vol: 0
+        }
+    ]
+};
