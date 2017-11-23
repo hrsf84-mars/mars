e051f1981e8e75454aab85e3a769a6fada83e16d
Tim Trautman <tctrautman@gmail.com>
2017-11-22 16:29:45 -0800

    Mostly adhere to Airbnb linter in Graph component

---
+++ components/Graph.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Label } from 'recharts';
+
+function Graph(props) {
+  return (
+    <div id="graph">
+      <LineChart width={800} height={400} data={props.data}>
+        <Line type="monotone" dataKey="primary_google_trends_vol" stroke="#8884d8" />
+        <Line type="monotone" dataKey="secondary_google_trends_vol" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="formattedAxisTime">
+          <Label value="Weeks after release date" offset={0} position="insideBottom" />
+        </XAxis>
+        <YAxis label={{ value: 'Search Volume', angle: -90, position: 'insideLeft' }} />
+      </LineChart>
+    </div>
+  );
+}
+
+export default Graph;
