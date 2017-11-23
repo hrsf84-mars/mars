0188d5bc480d23a270ef08cfb044697ce536b23d
Tim Trautman <tctrautman@gmail.com>
2017-11-22 19:53:55 -0800

    Mostly adhere to Airbnb guidelines in PrimaryTable component

---
+++ components/PrimaryTable.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function PrimaryTable(props) {
+  return (
+    <div id="table">
+      <div id="first_movie">
+        <h3>{props.primary_movie.title}</h3>
+        <p>Revenue: {props.primary_movie.revenue}</p>
+        <p>Release Date: {props.primary_movie.release_date}</p>
+        <p>Production Company: {props.primary_movie.production_company}</p>
+      </div>
+    </div>
+  );
+}
+
+export default PrimaryTable;
