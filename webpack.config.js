acbc5d3a5011d78410636a6f7bb788aaa01e4215
futamiyuuki <aa805766@gmail.com>
2017-11-25 16:54:39 -0800

    Include MaterialUI

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['env', 'react'],
+          presets: ['env', 'react', 'stage-1'],
         },
       },
     ],
