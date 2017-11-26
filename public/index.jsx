0211f6894291c9939b948c8d97c4be92cb117edb
futamiyuuki <aa805766@gmail.com>
2017-11-25 18:45:38 -0800

    Flip theme colors

--- public/index.jsx
+++ public/index.jsx
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import App from '../components/App';
 import reducers from '../reducers';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+  <MuiThemeProvider>
     <Provider store={createStoreWithMiddleware(reducers)}>
       <App />
     </Provider>
