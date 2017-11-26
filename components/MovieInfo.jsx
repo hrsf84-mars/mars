fd89f15e2179620e854e3589ae60f5f6750efeb5
futamiyuuki <aa805766@gmail.com>
2017-11-25 22:11:56 -0800

    Fix revenue and date format in MovieInfo

--- components/MovieInfo.jsx
+++ components/MovieInfo.jsx
@@ -7,6 +7,26 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
+import moment from 'moment';
+
+const translateToCurrency = (value) => {
+  let num = value;
+  let commaCounter = 0;
+  let res = '';
+  while (num > 0) {
+    const dig = num % 10;
+
+    commaCounter += 1;
+    if (commaCounter > 3) {
+      res = `,${res}`;
+      commaCounter %= 3;
+    }
+    res = dig + res;
+
+    num = Math.floor(num / 10);
+  }
+  return `$${res}`;
+};
 
 export default ({ primaryMovie, secondaryMovie }) => {
   const primaryTitle = primaryMovie.title;
@@ -29,13 +49,19 @@ export default ({ primaryMovie, secondaryMovie }) => {
       >
         <TableRow>
           <TableRowColumn>Revenue</TableRowColumn>
-          <TableRowColumn>{primaryMovie.revenue}</TableRowColumn>
-          {secondaryTitle && <TableRowColumn>{secondaryMovie.revenue}</TableRowColumn>}
+          <TableRowColumn>
+            {primaryMovie.revenue && translateToCurrency(primaryMovie.revenue)}
+          </TableRowColumn>
+          {secondaryTitle &&
+          <TableRowColumn>{translateToCurrency(secondaryMovie.revenue)}</TableRowColumn>}
         </TableRow>
         <TableRow>
           <TableRowColumn>Release Date</TableRowColumn>
-          <TableRowColumn>{primaryMovie.releaseDate}</TableRowColumn>
-          {secondaryTitle && <TableRowColumn>{secondaryMovie.releaseDate}</TableRowColumn>}
+          <TableRowColumn>
+            {primaryMovie.releaseDate && moment(primaryMovie.releaseDate).format('MMMM Do YYYY')}
+          </TableRowColumn>
+          {secondaryTitle &&
+          <TableRowColumn>{moment(secondaryMovie.releaseDate).format('MMMM Do YYYY')}</TableRowColumn>}
         </TableRow>
         <TableRow>
           <TableRowColumn>Genres</TableRowColumn>
