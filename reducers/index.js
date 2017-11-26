b6993d7903a0c153fee6c5890b070ca8e05eb45f
futamiyuuki <aa805766@gmail.com>
2017-11-25 21:13:45 -0800

    Add selected movie as Material UI Chip

--- reducers/index.js
+++ reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 import PrimaryMovieReducer from './primaryMovie';
 import SecondaryMovieReducer from './secondaryMovie';
 import GraphDataReducer from './graphData';
-import IsPrimarySelectedReducer from './isPrimarySelected';
 
 const rootReducer = combineReducers({
   primaryMovie: PrimaryMovieReducer,
   secondaryMovie: SecondaryMovieReducer,
   graphData: GraphDataReducer,
-  isPrimarySelected: IsPrimarySelectedReducer,
 });
 
 export default rootReducer;
