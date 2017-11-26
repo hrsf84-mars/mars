66b7d54e835d64e41fd7419c558b68ca6cff53ef
joe-nm <joenealmoore@gmail.com>
2017-11-25 16:50:56 -0800

    compliant with airbnb linter

--- utils/twitterEmotion.js
+++ utils/twitterEmotion.js
@@ -1,64 +1,69 @@
 const axios = require('axios');
 
-const twitter_api = 'https://api.twitter.com/1.1/search/tweets.json';
-const twitter_token = process.env.TWITTERAPI;
+const twitterApiUrl = 'https://api.twitter.com/1.1/search/tweets.json';
+const twitterToken = process.env.TWITTERAPI;
 
-const watson_api = 'https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze';
-const watson_token = process.env.WATSONAPI;
+const watsonApiUrl = 'https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze';
+const watsonToken = process.env.WATSONAPI;
 
-var getEmotions = function(text) {
-  return axios.get(watson_api,
-    { params:  {
-      version: '2017-02-27',
-      features: 'emotion',
-      text: text
+const getEmotions = function (text) {
+  return axios.get(
+    watsonApiUrl,
+    {
+      params: {
+        version: '2017-02-27',
+        features: 'emotion',
+        text,
+      },
+      headers: {
+        Authorization: `Basic ${watsonToken}`,
+      },
     },
-    headers: {
-      Authorization: "Basic " + watson_token
-    }
-  })
-  .catch(err => {
-    console.log('error from watson API:  ' + err);
-  }).then(Promise.resolve(false));
+  )
+    .catch((err) => {
+      console.log(`error from watson API:  ${err}`);
+    }).then(Promise.resolve(false));
 };
 
-//returns an object with emotion properties
-module.exports.avgTweetEmotion = function(twitterSearchTerm) {
-  return axios.get(twitter_api,
-    {params: {
-      q: twitterSearchTerm
+//  returns an object with emotion properties
+module.exports.avgTweetEmotion = function (twitterSearchTerm) {
+  return axios.get(
+    twitterApiUrl,
+    {
+      params: {
+        q: twitterSearchTerm,
+      },
+      headers: {
+        Authorization: `Bearer ${twitterToken}`,
+      },
     },
-    headers: {
-      Authorization: "Bearer " + twitter_token
-    }
-  })
-  .then(res => {
-    return res.data.statuses.map( status => {
-      return status.text;
-    });
-  })
-  .then(texts => {
-    var emotions = [];
-    texts.forEach( text => emotions.push(getEmotions(text)) );
-    return Promise.all(emotions);
-  })
-  .then( emotions => {
-    var avg_emotion = {sadness: 0, joy: 0, fear: 0, disgust: 0, anger: 0};
-    var emotion_count = 0;
-    emotions.forEach( emotion => {
-      if(emotion) {
-        for(prop in avg_emotion) {
-          avg_emotion[prop] += emotion.data.emotion.document.emotion[prop];
-        }
-        emotion_count += 1;
-      }
+  )
+    .then(res => res.data.statuses.map(status => status.text))
+    .then((texts) => {
+      const emotions = [];
+      texts.forEach(text => emotions.push(getEmotions(text)));
+      return Promise.all(emotions);
     })
-    for(prop in avg_emotion) {
-      avg_emotion[prop] /= emotion_count;
-    }
-    return avg_emotion;
-  })
-  .catch(err => {
-    console.log('error from twitter api:  ' + err);
-  });
-}
+    .then((emotions) => {
+      const avgEmotion = {
+        sadness: 0, joy: 0, fear: 0, disgust: 0, anger: 0,
+      };
+      const props = Object.keys(avgEmotion);
+      let emotionCount = 0;
+      emotions.forEach((emotion) => {
+        if (emotion) {
+          for (let i = 0; i < props.length; i += 1) {
+            avgEmotion[props[i]] += emotion.data.emotion.document.emotion[props[i]];
+          }
+          emotionCount += 1;
+        }
+      });
+      for (let i = 0; i < props.length; i += 1) {
+        avgEmotion[props[i]] /= emotionCount;
+      }
+      return avgEmotion;
+    })
+    .catch((err) => {
+      console.log(`error from twitter api:  ${err}`);
+    });
+};
