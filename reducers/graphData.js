a13de48a559386de70cbce6ea1ca58e3bcd89d1a
futamiyuuki <aa805766@gmail.com>
2017-11-25 16:54:39 -0800

    Include MaterialUI

--- reducers/graphData.js
+++ reducers/graphData.js
@@ -33,7 +33,7 @@ function combineTwoLines(primaryGraph, secondaryGraph) {
   }
 
   res.sort((a, b) => (new Date(a.date) <= new Date(b.date) ? -1 : 1));
-  console.log(res);
+  // console.log(res);
 
   return res;
 }
