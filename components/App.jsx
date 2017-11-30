70a3847ec60b8b4cf70de0b18d37fba9cf44a327
Michael Neveu <mbneveu@gmail.com>
2017-11-29 23:32:32 -0800

    basic react rounter up and running

--- components/App.jsx
+++ components/App.jsx
@@ -4,15 +4,79 @@ import AppBar from 'material-ui/AppBar';
 import SearchBox from '../containers/SearchBox';
 import MovieDetail from '../containers/MovieDetail';
 
-export default function App() {
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import Settings from './loggedin/settings.jsx';
+import SavedSearches from './loggedin/saved.jsx';
+import LoggedInHomePage from './loggedin/loggedinhome.jsx';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+
+
+var App = () => {
   return (
     <Paper>
-      <AppBar
-        title="Cliff Movie DB"
-        iconElementLeft={<img src="https://s3.amazonaws.com/tt-public-assets/Cliff_icon.png" alt="Logo" />}
-      />
-      <SearchBox />
-      <MovieDetail />
+      <Router>
+        <div>
+          <AppBar
+            title="DEMI Movie DB"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          > 
+            <IconMenu
+                iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+                anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+                targetOrigin={{horizontal: 'left', vertical: 'top'}}
+              >
+              <Link to="/settings">Test Click Here</Link>
+              <Link to="/">Home</Link>
+              <MenuItem primaryText="Home Test" />
+              <MenuItem primaryText="Settings Page" containerElement={<Link to="/settings" />}/>
+              <MenuItem primaryText="Saved Searches" containerElement={<Link to="/saved" />}/>
+              <MenuItem primaryText="LogIn" containerElement={<Link to="/loggedinhome" />}/>
+            </IconMenu>
+          </AppBar>
+          <hr/>
+          <Route exact path="/" component={Core}/>
+          <Route path="/settings" component={Settings}/>
+          <Route path="/saved" component={SavedSearches}/> 
+          <Route path="/loggedinhome" component={LoggedInHomePage}/>    
+        </div>
+      </Router>
     </Paper>
   );
 }
+
+
+
+class Core extends React.Component {
+  constructor() {
+    super();
+  }
+
+  onMenuClicked() {
+    console.log('clicked');
+    //have a state for each of the menu items
+    //change the state
+  }
+
+  render() {
+    return (
+      <Paper>
+          <SearchBox />
+          <MovieDetail />
+      </Paper>
+    );
+  }
+}
+
+
+
+export default App;
+
+
---
+++ components/loggedin/saved.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+var SavedSearches = () => (
+  <div>
+    <h1>
+      Saved Searches Page Rendered
+    </h1>
+  </div>
+);
+
+
+
+
+
+export default SavedSearches;
